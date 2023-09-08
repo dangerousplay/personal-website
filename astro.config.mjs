079614845381f972845574fdf5794a2bd5d0eedd
Dangerousplay <dangerousplay715@gmail.com>
2023-09-07 21:40:51 -0300

    [Added] Mathjax support for blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import image from "@astrojs/image";
+import remarkMath from "remark-math";
+import rehypeMathjaxChtml from 'rehype-mathjax/chtml.js'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +17,22 @@ export default defineConfig({
       cacheDir: "./.cache/image",
       logLevel: 'debug',
     }
-  )]
+  )],
+    markdown: {
+        extendDefaultPlugins: true,
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [
+            [rehypeMathjaxChtml, {
+                chtml: {
+                    fontURL: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2'
+                }
+            }]
+        ],
+        shikiConfig: {
+            theme: 'github-light',
+            langs: [],
+            // Enable word wrap to prevent horizontal scrolling
+            wrap: true
+        }
+    }
 });
